236fac78670824f17eb6ac09594cbc9d2f3a5541
MANAS <manasrathore2342@gmail.com>
2022-02-22 01:26:09 +0530

    check-1

--- realestate_app/settings.py
+++ realestate_app/settings.py
@@ -15,7 +15,9 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#? BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # #Authentication backends
@@ -170,12 +172,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 # LOCATION OF STATIC DIR
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "modules_store"),
     
     os.path.join(BASE_DIR, "realestate_app/static"),
     os.path.join(BASE_DIR, "styles"),
-]
+)
 
 DEBUG=False
 
