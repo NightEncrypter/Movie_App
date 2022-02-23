fc9c7548df6f7cf59ae3a6d5c8b2046eb027a917
MANAS <manasrathore2342@gmail.com>
2022-02-23 17:25:17 +0530

    ui

--- watchlists/urls.py
+++ watchlists/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     
     path("",views.watchlistData,name="watchlist"),
     path("addinwatchlist",views.addInWatchlist,name="addwatchlist"),
+    path("addinwatchlist_2",views.addInWatchlist_2,name="addwatchlist_2"),
 ]
--- watchlists/views.py
+++ watchlists/views.py
@@ -49,5 +49,32 @@ def addInWatchlist(request):
                 messages.error(request,"Please login and then try")
                 return redirect("login")
         
+def addInWatchlist_2(request):
+    # print(w_id)
+    if request.user.is_authenticated:
+        
+        if request.method=="POST":
+            # request.POST
+            print(request.POST)
+            movie_id=request.POST["movie_id"]
+            user_id=request.user.id
+            
+            if Watchlist.objects.filter(movie_id=movie_id ,   user_id=user_id):
+                messages.error(request,"Already added in a  watchlist")
+                return redirect("movie",movie_id)    
+                
+            else:
+                movie_instance=Movie.objects.get(id=movie_id)
+                user_instance=User.objects.get(id=user_id)
+                addinwatchlist=Watchlist.objects.create(movie=movie_instance,user=user_instance)
+                addinwatchlist.save()
+                
+                print(addinwatchlist.movie.id)
+                messages.success(request,"Your movie successfully added in watchlist")
+                return redirect("movie",movie_id) 
+        else:
+                messages.error(request,"Please login and then try")
+                return redirect("login")
+        
 def getWatchlist():
     return 
